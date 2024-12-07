9aa31ca8e9d50f91a7e304c044180c1f3db26321
Yassin Rian <yassinrian@hotmail.com>
2024-12-07 01:22:04 +0100

    latest

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ define(function () {
 
 	BasicControl.prototype.draw = function (oControlHost) {
 		const cont = oControlHost.container;
-		cont.innerHTML = "<h1>halloooo!!</h1>";
+		cont.innerHTML = "<h1>ok is dit de latest!!</h1>";
 	};
 	return BasicControl;
 });
