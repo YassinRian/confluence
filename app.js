4d87c5a322757a6d64ded9621b4dd69123e8627c
YassinRian <yassinrian@hotmail.com>
2024-12-07 15:13:54 +0100

    rommelen met github

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ define(function () {
 
 	BasicControl.prototype.draw = function (oControlHost) {
 		const cont = oControlHost.container;
-		cont.innerHTML = "<h1>hallo op deze mooie zaterdag</h1>";
+		cont.innerHTML = "<h1>hallo op deze mooie zaterdag en ik ben aan het rommelen met Github</h1>";
 	};
 	return BasicControl;
 });
