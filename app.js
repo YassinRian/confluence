21c6d51eb28042495828c8f42675eda05ee26cb0
YassinRian <yassinrian@hotmail.com>
2024-12-07 14:42:36 +0100

    mooie zaterdag

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ define(function () {
 
 	BasicControl.prototype.draw = function (oControlHost) {
 		const cont = oControlHost.container;
-		cont.innerHTML = "<h1>hallo Iman en khalid!!</h1>";
+		cont.innerHTML = "<h1>hallo op deze mooie zaterdag</h1>";
 	};
 	return BasicControl;
 });
